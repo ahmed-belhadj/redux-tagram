a560501628f11a8fbedaf82c2caef60603069fb4
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-08-04 15:53:10 -0500

    react-router

--- app/client/reduxtagram.js
+++ app/client/reduxtagram.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
 import Header from "./components/Header";
+import Post from "./components/Post";
+import Feed from "./components/Feed";
 
 import css from "./styles/style.styl";
 
-render(<Header />, document.getElementById("root"));
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Header}>
+      <IndexRoute component={Feed} />
+      <Route path="/view/:postId" component={Post} />
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById("root"));
